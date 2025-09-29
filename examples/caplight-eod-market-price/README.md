b637b714894673760a4fcdaec0cd6866bd48317c
gluax <16431709+gluax@users.noreply.github.com>
2025-09-29 12:09:23 -0700

    refactor(caplight): handle utc hour

--- examples/caplight-eod-market-price/README.md
+++ examples/caplight-eod-market-price/README.md
@@ -1,9 +1,9 @@
 # Caplight End of Day Market Price
 
 Deployments:
-- [Testnet](https://testnet.explorer.seda.xyz/oracle-programs/58e9a7d1c7597e9580f4f44f4e64e3946bff70868f2a6e164da6cfe340a586ee): Non String Result
-- [Testnet](https://testnet.explorer.seda.xyz/oracle-programs/a30be1fcb79ca173878f816f3f4bdc73be4511ceb792dd60dbb6db5faba9f50b): String Result
-<!-- - [Mainnet]() -->
+- [Testnet](https://testnet.explorer.seda.xyz/oracle-programs/93e349bc67017b4c62696d5e96ec7e7dce6e3ef679b930b0a219a08fee8f86fb): Non String Result
+- [Testnet](https://testnet.explorer.seda.xyz/oracle-programs/5e42846397786ccb3fdbce7d5b786585b635703ae240cc45eeeaa16a2d7a45b7): String Result
+- [Mainnet](https://explorer.seda.xyz/oracle-programs/bb688e6c356014cda17d55662d614149050630bd30507f29045868358a38aa8b): String Result
 
 ## Overview
 
@@ -12,14 +12,23 @@ This Oracle Program fetches the latest market data returned from the [Caplight A
 You can test this Oracle Program on testnet with the following command:
 
 ```sh
-cargo post-dr caplight-eod-market-price 54782-29 -i 58e9a7d1c7597e9580f4f44f4e64e3946bff70868f2a6e164da6cfe340a586ee -r 3
+cargo post-dr caplight-eod-market-price 54782-29 -i 93e349bc67017b4c62696d5e96ec7e7dce6e3ef679b930b0a219a08fee8f86fb -r 3
 ```
 
 Or for a version that posts the result as a string you can do:
 
 ```sh
-cargo post-dr caplight-eod-market-price --str-result 54782-29 -i a30be1fcb79ca173878f816f3f4bdc73be4511ceb792dd60dbb6db5faba9f50b
+cargo post-dr caplight-eod-market-price --str-result 54782-29 -i 5e42846397786ccb3fdbce7d5b786585b635703ae240cc45eeeaa16a2d7a45b7
 ```
+
+You can optionally manually post which Which Hour you want the data from:
+
+```sh
+cargo post-dr caplight-eod-market-price --str-result 54782-29,4 -i 5e42846397786ccb3fdbce7d5b786585b635703ae240cc45eeeaa16a2d7a45b7
+```
+
+> [!NOTE] The UTC HOUR does NOT effect testnet data.
+> Valid UTC Hour values are 0,4,8,12,16, and 20.
 
 ## Execution Phase:
 
@@ -31,12 +40,20 @@ The Execution Phase expects the `pitchbookId` of the company.
 
 1. Validates the Data Request execution argument is not empty.
 2. Makes an HTTP call to the Caplight Data Proxy.
-3. Converts the decimal to a `u128` with 2 decimal precision.
+3. Converts the decimal to a `u128` with 4 decimal precision.
 4. Returns the `u128` in little endian format.
 
 ### Example
 
+#### No UTC Hour(Defaults to the Caplight API defined default)
+
 Input: `54782-29`
+
+Output: `4221`
+
+#### With UTC Hour
+
+Input: `54782-29,4`
 
 Output: `4221`
 
--- examples/caplight-eod-market-price/src/execution_phase.rs
+++ examples/caplight-eod-market-price/src/execution_phase.rs
@@ -3,12 +3,12 @@ use anyhow::Result;
 use seda_sdk_rs::{Process, elog, log, proxy_http_fetch};
 
 #[cfg(feature = "testnet")]
-const API_URL: &str = "http://104.155.34.32:5384/proxy/";
+const API_URL: &str = "http://104.155.34.32:5384/proxy/market-price-fixed-eod?";
 #[cfg(feature = "testnet")]
 const PROXY_PUBLIC_KEY: &str = "0306346975352e34719df41928048482b285d24cd27f8e5fc2df7e4095f9cc14cf";
 
 #[cfg(feature = "mainnet")]
-const API_URL: &str = todo!("http://34.14.120.47:5384/proxy/");
+const API_URL: &str = "http://34.14.120.47:5384/proxy/market-price-fixed-eod?";
 #[cfg(feature = "mainnet")]
 const PROXY_PUBLIC_KEY: &str = "02088452cd5025f33d7ce95ee8eb7ba34b94b518ea23b1897665e1afdbcae2ca18";
 
@@ -36,9 +36,8 @@ pub fn execution_phase() -> Result<()> {
 pub fn execution_phase() -> Result<()> {
     use seda_sdk_rs::{HttpFetchMethod, HttpFetchOptions};
 
-    // Expected to be a valid project Id for the Caplight API.
+    // Expected to be a valid project Id and optionally a comma separated valid value for utcHour(0, 4, 8, 12, 16, 20) for the Caplight API.
     let dr_inputs_raw = String::from_utf8(Process::get_inputs())?;
-
     // If no input is provided, log an error and return.
     if dr_inputs_raw.is_empty() {
         elog!("No input provided for the equity price request.");
@@ -46,10 +45,33 @@ pub fn execution_phase() -> Result<()> {
         return Ok(());
     }
 
-    // Log the asset being fetched as part of the Execution Standard Out.
-    log!("Fetching price for: {dr_inputs_raw}");
+    let parts: Vec<&str> = dr_inputs_raw.split(',').collect();
+    let (pitchbook_id, utc_hour) = match parts.as_slice() {
+        // validate that each field is in the valid fields list
+        [pitchbook_id, utc_hour] => match *utc_hour {
+            "0" | "4" | "8" | "12" | "16" | "20" => (pitchbook_id, Some(utc_hour)),
+            _ => {
+                elog!("Invalid UTC hour format: should be one of 0,4,8,12,16,20");
+                Process::error("Invalid UTC hour format".as_bytes());
+                return Ok(());
+            }
+        },
+        [pitchbook_id] => (pitchbook_id, None),
+        _ => {
+            elog!("Invalid input format");
+            Process::error("Invalid input format".as_bytes());
+            return Ok(());
+        }
+    };
 
-    let url = [API_URL, &dr_inputs_raw].concat();
+    // Log the asset being fetched as part of the Execution Standard Out.
+    log!("Fetching price for: {pitchbook_id}, utc_hour: {utc_hour:?}");
+
+    let url = if let Some(utc_hour) = utc_hour {
+        [API_URL, "pitchbookId=", pitchbook_id, "?utcHour=", utc_hour].concat()
+    } else {
+        [API_URL, "pitchbookId=", pitchbook_id].concat()
+    };
     let response = proxy_http_fetch(
         url,
         Some(PROXY_PUBLIC_KEY.to_string()),
